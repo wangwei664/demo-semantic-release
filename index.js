fd75aa6d82fed63a4dc031c36c0353afef23656a
wangwei3 <wangwei3@liepin.com>
2024-02-01 13:34:24 +0800

    feat: CI token 权限调试

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
-console.log("semantic-release 配置提交通过了");
-
-console.log('实际发布时会根据自己配置的 plugin 执行发布操作');
+const arr = [
+    'semantic-release 配置提交通过了',
+    '实际发布时会根据自己配置的 plugin 执行发布操作',
+];
+console.log(arr);
